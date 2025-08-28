dfd32e57512ac58908a0b1ea858439853b3400d6
Colin LAURENT <colin69340@gmail.com>
2025-08-28 22:20:08 +0200

    [~] Update requirements & Strategy correction

--- src/marketprediction/plots.py
+++ src/marketprediction/plots.py
@@ -1,14 +1,28 @@
 import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
 
+
 def plot_return(wallet, data, tickers):
-    strategy_return = [np.log(wallet['Total'][i+1] / wallet['Total'][i]) for i in range(len(wallet['Total']) - 1)]
-    market_return = {}
-    for ticker in tickers:
-        market_return[ticker] = np.log(data[f'Close {ticker}'].shift(-1) / data[f'Close {ticker}'])
+    total_wallet = pd.Series(wallet['Total'])
+    strategy_return = np.log(total_wallet / total_wallet.shift(1))
+    market_return = {
+        ticker: (np.log(
+            data[f'Close {ticker}'] / data[f'Close {ticker}'].shift(1)
+        ))
+        for ticker in tickers
+    }
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data.index, y=strategy_return, name="Strategy Return"))
+    fig.add_trace(go.Scatter(
+        x=data.index[1:],
+        y=strategy_return[1:],
+        name="Strategy Return"
+    ))
     for ticker in tickers:
-        fig.add_trace(go.Scatter(x=data.index, y=market_return[ticker], name=f'Return {ticker}'))
+        fig.add_trace(go.Scatter(
+            x=data.index[1:],
+            y=market_return[ticker][1:],
+            name=f'Return {ticker}'
+        ))
     fig.update_layout(title='Returns')
     fig.show()
--- src/marketprediction/utils.py
+++ src/marketprediction/utils.py
@@ -1,8 +1,8 @@
 class Strategy:
     def generate_signals(self, data, tickers):
         """Return a column 'signal' : 1 -> buy ; 0 -> hold ; -1 -> sell"""
-        raise NotImplementedError("Each Strategy must implement generate_signals")
-    
+        raise NotImplementedError("Strategy must implement generate_signals")
+
 
 class TrendFollowingStrategy(Strategy):
     def __init__(self, ma_short=5, ma_long=20):
@@ -11,14 +11,35 @@ class TrendFollowingStrategy(Strategy):
 
     def generate_signals(self, data, tickers):
         for ticker in tickers:
-            data[f'MA_short {ticker}'] = data[f'Close {ticker}'].shift(1).rolling(window=self.ma_short).mean()
-            data[f'MA_long {ticker}'] = data[f'Close {ticker}'].shift(1).rolling(window=self.ma_long).mean()
+            data[f'MA_short {ticker}'] = (
+                data[f'Close {ticker}']
+                .shift(1)
+                .rolling(window=self.ma_short)
+                .mean()
+            )
+            data[f'MA_long {ticker}'] = (
+                data[f'Close {ticker}']
+                .shift(1)
+                .rolling(window=self.ma_long)
+                .mean()
+            )
             data[f'Signal {ticker}'] = 0
-            buy_mask = (data[f'MA_short {ticker}'] > data[f'MA_long {ticker}']) & (data[f'MA_short {ticker}'].shift(1) <= data[f'MA_long {ticker}'].shift(1))
-            sell_mask = (data[f'MA_short {ticker}'] < data[f'MA_long {ticker}']) & (data[f'MA_short {ticker}'].shift(1) >= data[f'MA_long {ticker}'].shift(1))
+            buy_mask = (
+                (data[f'MA_short {ticker}'] > data[f'MA_long {ticker}'])
+                &
+                (data[f'MA_short {ticker}'] <= data[f'MA_long {ticker}'])
+                .shift(1)
+            )
+            sell_mask = (
+                (data[f'MA_short {ticker}'] < data[f'MA_long {ticker}'])
+                &
+                (data[f'MA_short {ticker}'] >= data[f'MA_long {ticker}'])
+                .shift(1)
+            )
             data.loc[buy_mask, f'Signal {ticker}'] = 1
             data.loc[sell_mask, f'Signal {ticker}'] = -1
         return data
+
 
 class MovingAverageStrategy(Strategy):
     def __init__(self, ma_short=5, ma_long=30, thresh=0.9):
@@ -28,18 +49,49 @@ class MovingAverageStrategy(Strategy):
 
     def generate_signals(self, data, tickers):
         for ticker in tickers:
-            data[f'MA_short {ticker}'] = data[f'Close {ticker}'].shift(1).rolling(window=self.ma_short).mean()
-            data[f'MA_long {ticker}'] = data[f'Close {ticker}'].shift(1).rolling(window=self.ma_long).mean()
+            data[f'MA_short {ticker}'] = (
+                data[f'Close {ticker}']
+                .shift(1)
+                .rolling(window=self.ma_short)
+                .mean()
+            )
+            data[f'MA_long {ticker}'] = (
+                data[f'Close {ticker}']
+                .shift(1)
+                .rolling(window=self.ma_long)
+                .mean()
+            )
             data[f'Signal {ticker}'] = 0
-            buy_mask = (data[f'MA_short {ticker}'] < self.thresh * data[f'MA_long {ticker}']) & (data[f'MA_short {ticker}'].shift(1) >= self.thresh * data[f'MA_long {ticker}'].shift(1))
-            sell_mask = (data[f'MA_short {ticker}'] > (2 - self.thresh) * data[f'MA_long {ticker}']) & (data[f'MA_short {ticker}'].shift(1) <= (2 - self.thresh) * data[f'MA_long {ticker}'].shift(1))
+            buy_mask = (
+                (data[f'MA_short {ticker}'] <
+                 self.thresh * data[f'MA_long {ticker}'])
+                &
+                (data[f'MA_short {ticker}'] >=
+                 self.thresh * data[f'MA_long {ticker}'])
+                .shift(1)
+            )
+            sell_mask = (
+                (data[f'MA_short {ticker}'] >
+                 (2 - self.thresh) * data[f'MA_long {ticker}'])
+                &
+                (data[f'MA_short {ticker}']
+                 <= (2 - self.thresh) * data[f'MA_long {ticker}'])
+                .shift(1)
+            )
             data.loc[buy_mask, f'Signal {ticker}'] = 1
             data.loc[sell_mask, f'Signal {ticker}'] = -1
         return data
-    
-    
+
+
 class Backtester:
-    def __init__(self, tickers, data, strategy, hold_max=14, initial_capital=1000):
+    def __init__(
+        self,
+        tickers,
+        data,
+        strategy,
+        hold_max=14,
+        initial_capital=1000
+    ):
         """
         Args:
             hold_max [int] : maximal number of days before selling a position
@@ -64,16 +116,26 @@ class Backtester:
             current_cash = self.wallet['Cash'][-1]
             for ticker in self.tickers:
                 current_pos = self.wallet[ticker][-1]
-                if (row[f'Signal {ticker}'] == 1) and (current_cash >= row[f'Open {ticker}']):
+                if (
+                    (row[f'Signal {ticker}'] == 1) and
+                    (current_cash >= row[f'Open {ticker}'])
+                ):
                     current_cash -= row[f'Open {ticker}']
-                    self.wallet[ticker].append([current_pos[0] + 1, current_pos[1] + row[f'Open {ticker}']])
+                    self.wallet[ticker].append(
+                        [current_pos[0] + 1,
+                         current_pos[1] + row[f'Open {ticker}']]
+                    )
                 elif (row[f'Signal {ticker}'] == -1) and (current_pos[0] > 0):
                     current_cash += row[f'Open {ticker}']
-                    self.wallet[ticker].append([current_pos[0] - 1, current_pos[1] - row[f'Open {ticker}']])
+                    self.wallet[ticker].append(
+                        [current_pos[0] - 1,
+                         current_pos[1] - row[f'Open {ticker}']]
+                    )
                 else:
                     self.wallet[ticker].append(current_pos)
 
-                self.wallet[ticker][-1][1] = self.wallet[ticker][-1][0] * row[f'Close {ticker}']
+                self.wallet[ticker][-1][1] = (self.wallet[ticker][-1][0] *
+                                              row[f'Close {ticker}'])
                 current_total += self.wallet[ticker][-1][1]
 
             self.wallet['Cash'].append(current_cash)
